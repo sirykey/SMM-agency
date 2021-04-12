c87f8674ee37068d30ab73832b44f1495878fedb
SiriKey <kiryu110@mail.ru>
2021-04-12 14:00:04 +0200

    added routs

--- src/app/App.js
+++ src/app/App.js
@@ -3,14 +3,19 @@ import { CssBaseline } from '@material-ui/core';
 import Admin from '../features/adminPage/index';
 import React from 'react';
 import SignUp from '../features/auth/signUp';
+import { Route, Switch } from 'react-router';
+import Content from '../features/content/Content';
 
 function App() {
   return (
     <div className="container">
-      {/*<Auth />*/}
-      {/*<Admin />*/}
-      <SignUp />
-      <CssBaseline />
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/content" component={Content} />
+        <Route path="/registration" component={SignUp} />
+        {/*<Admin />*/}
+        <CssBaseline />
+      </Switch>
     </div>
   );
 }
---
+++ src/features/content/Content.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Content(props) {
+  return <div>123</div>;
+}
+
+export default Content;
