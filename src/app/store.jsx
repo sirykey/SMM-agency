bbe16c7fce7774291efd4529167cb99cd63c76d1
SiriKey <kiryu110@mail.ru>
2021-04-14 12:36:31 +0200

    added slices

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from '../features/auth/authSlice';
+import headersSlice from '../features/headers/headersSlice';
+import adminSlice from '../features/adminPage/adminSlice';
 
 export const store = configureStore({
   reducer: {
     authSlice,
+    headersSlice,
+    adminSlice,
   },
 });
---
+++ src/features/headers/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Headers(props) {
+  return <div></div>;
+}
+
+export default Headers;
