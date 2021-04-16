4a13e7594c72d8c21ae88537d4f112b209efe2ef
SiriKey <kiryu110@mail.ru>
2021-04-16 15:49:17 +0200

    added delete button

--- src/features/headers/Header.jsx
+++ src/features/headers/Header.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteHeader } from './headersSlice';
 
-function Header(props) {
-  return <div>{props.header.text}</div>;
+function Header({ header }) {
+  const dispatch = useDispatch();
+
+  const handleDeleteClick = (_id) => {
+    dispatch(deleteHeader(_id));
+  };
+  return (
+    <div>
+      {header.text}
+      <button onClick={handleDeleteClick}>delete</button>
+    </div>
+  );
 }
 
 export default Header;
