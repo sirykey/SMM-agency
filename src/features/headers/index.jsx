415a528b8fff562a1b206355b1f08cc63eb39ea2
SiriKey <kiryu110@mail.ru>
2021-04-30 15:26:49 +0200

    added new input for headers

--- src/features/headers/index.jsx
+++ src/features/headers/index.jsx
@@ -8,17 +8,26 @@ function Headers(props) {
   const headers = useSelector((state) => state.headersSlice.items);
   console.log(headers);
   const [inputValue, setInputValue] = useState('');
+  const [textValue, setTextValue] = useState('');
 
   useEffect(() => {
     dispatch(fetchHeaders());
   }, []);
 
   const handleAddClick = (text) => {
-    dispatch(addHeader(text));
+    dispatch(
+      addHeader({
+        title: inputValue,
+        text: textValue,
+      }),
+    );
   };
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
+  };
+  const handleTextChange = (e) => {
+    setTextValue(e.target.value);
   };
 
   return (
@@ -27,6 +36,9 @@ function Headers(props) {
         return <Header key={header._id} header={header} />;
       })}
       <input type="text" value={inputValue} onChange={handleInputChange} />
+      <br />
+      <input type="text" value={textValue} onChange={handleTextChange} />
+      <br />
       <button onClick={handleAddClick}>Add</button>
     </div>
   );
