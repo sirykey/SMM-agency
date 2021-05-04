183c54bd3ee5ecc6a91ca7f061ff7dcba467969c
SiriKey <kiryu110@mail.ru>
2021-05-04 16:52:14 +0200

    little changes

--- src/features/users/Header.jsx
+++ src/features/users/Header.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
 import { AccountCircle } from '@material-ui/icons';
 import { useMainStyles } from './styles';
+import AdminMenu from './admin/AdminMenu';
 
 function Header(props) {
   const classes = useMainStyles();
   return (
     <AppBar position="absolute" className={classes.appBar}>
-      <Toolbar className={classes.toolbarIcon}>
+      <Toolbar className={classes.toolbar}>
         <Typography variant="h6" noWrap className={classes.title}>
           Smm-agency
         </Typography>
-        <IconButton color="inherit" edge="end" aria-label="open drawer">
-          <AccountCircle />
-        </IconButton>
+        <AdminMenu />
       </Toolbar>
     </AppBar>
   );
