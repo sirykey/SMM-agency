2f17779c330c12c25e5a2f8d119a0076f7066d2c
emin <Magamedi98@mail.ru>
2021-05-06 17:29:14 +0300

    added map for posts and drafts

---
+++ src/features/content/posts/post.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Hidden from '@material-ui/core/Hidden';
+import { useHistory } from 'react-router-dom';
+
+const useStyles = makeStyles({
+  card: {
+    display: 'flex',
+  },
+  cardDetails: {
+    flex: 1,
+  },
+  cardMedia: {
+    width: 160,
+  },
+});
+
+function Post({ post }) {
+  const classes = useStyles();
+  const history = useHistory();
+
+  return (
+    <Grid item xs={12} md={6}>
+      <CardActionArea
+        component="a"
+        onClick={() => history.push('/agency/blog')}
+      >
+        <Card className={classes.card}>
+          <div className={classes.cardDetails}>
+            <CardContent>
+              <Typography component="h2" variant="h5">
+                {post.title}
+              </Typography>
+              <Typography variant="subtitle1" color="textSecondary">
+                {post.date}
+              </Typography>
+              <Typography variant="subtitle1" paragraph>
+                {post.text}
+              </Typography>
+              <Typography variant="subtitle1" color="primary">
+                Continue reading...
+              </Typography>
+            </CardContent>
+          </div>
+          <Hidden xsDown>
+            <CardMedia
+              className={classes.cardMedia}
+              image={post.image !== undefined ? post.image : ''}
+              title={post.title}
+            />
+          </Hidden>
+        </Card>
+      </CardActionArea>
+    </Grid>
+  );
+}
+
+Post.propTypes = {
+  post: PropTypes.object,
+};
+
+export default Post;
