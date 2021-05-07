9625944019d2c0047e31d7d5f2c4acf0832328c2
SiriKey <kiryu110@mail.ru>
2021-05-07 23:05:08 +0200

    added some stuff

--- src/features/users/index.jsx
+++ src/features/users/index.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useMainStyles } from './styles';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Routs from './Routs';
+import { useDispatch } from 'react-redux';
+import { fetchUsers } from './usersSlice';
+import { fetchHeaders } from '../content/contentSlice';
 
 function MainPage(props) {
   const classes = useMainStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+    dispatch(fetchHeaders());
+  }, [dispatch]);
 
   return (
     <div className={classes.root}>
