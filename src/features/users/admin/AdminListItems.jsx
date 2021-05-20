4b600cc5e4af2190554ae2eb167b12c63dac03be
emin <Magamedi98@mail.ru>
2021-05-20 14:11:26 +0300

    Revert "small fixes"
    This reverts commit 08a2982c

--- src/features/users/admin/AdminListItems.jsx
+++ src/features/users/admin/AdminListItems.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
@@ -6,23 +6,18 @@ import Divider from '@material-ui/core/Divider';
 import DeleteIcon from '@material-ui/icons/Delete';
 import IconButton from '@material-ui/core/IconButton';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import BorderColorIcon from '@material-ui/icons/BorderColor';
+import EditIcon from '@material-ui/icons/Edit';
 import { Avatar } from '@material-ui/core';
 import { useProfileStyles } from '../styles';
 import { useDispatch } from 'react-redux';
 import { deleteUsers } from '../usersSlice';
-import DeleteUser from '../DeleteUser';
 
 function AdminListItems(props) {
   const classes = useProfileStyles();
-  const [open, setOpen] = useState(false);
-
-  const handleDeleteAlertOpen = () => {
-    setOpen(true);
-  };
-  const handleDeleteAlertClose = () => {
-    setOpen(false);
-  };
+  const dispatch = useDispatch();
+  const handleDeleteRedactor = (id) => {
+    dispatch(deleteUsers(id))
+  }
 
   return (
     <div>
@@ -31,28 +26,19 @@ function AdminListItems(props) {
           <Avatar></Avatar>
         </ListItemAvatar>
         <ListItemText
-          primary={${props.user.name} ${props.user.surname}}
+          primary={`${props.user.name} ${props.user.surname}`}
           secondary="редактор"
         />
         <ListItemSecondaryAction>
-          <IconButton className={classes.btn} edge="end" aria-label="changed">
-            <BorderColorIcon />
+          <IconButton className={classes.btn} edge="end" aria-label="changed" color="primary">
+            <EditIcon />
           </IconButton>
-          <IconButton
-            edge="end"
-            aria-label="delete"
-            onClick={handleDeleteAlertOpen}
-          >
+          <IconButton edge="end" aria-label="delete" color="secondary" onClick={() => handleDeleteRedactor(props.user._id)}>
             <DeleteIcon />
           </IconButton>
         </ListItemSecondaryAction>
       </ListItem>
       <Divider variant="inset" component="li" />
-      <DeleteUser
-        open={open}
-        handleClose={handleDeleteAlertClose}
-        id={props.user._id}
-      />
     </div>
   );
 }
