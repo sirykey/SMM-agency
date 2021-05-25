1d5b40a6e88476f7168df91d7a3a6484bf5e1b8f
emin <Magamedi98@mail.ru>
2021-05-25 13:44:53 +0300

    added editing and deleting in taskSlice.js

--- src/features/tasks/taskSlice.js
+++ src/features/tasks/taskSlice.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { api } from '../../app/api';
+import { deleteDraft } from '../content/contentSlice';
 
 export const fetchOneWorkerTasks = createAsyncThunk(
   'tasks/fetchTasks',
@@ -19,16 +20,40 @@ export const fetchAllTasks = createAsyncThunk(
   },
 );
 
-export const addTasks = createAsyncThunk(
-  'tasks/addTasks',
+export const addTask = createAsyncThunk(
+  'tasks/addTask',
   async ({ message, id }) => {
     const response = await api.post(`/users/${id}/tasks`, {
       message: message,
     });
 
-    console.log(response);
-
     return response.data;
+  },
+);
+
+export const editTask = createAsyncThunk(
+  'tasks/editTask',
+  async ({ message, id }, thunkAPI) => {
+    try {
+      await api.patch(`/tasks/${id}`, { message });
+
+      return id;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  },
+);
+
+export const deleteTask = createAsyncThunk(
+  'tasks/deleteTask',
+  async (id, thunkAPI) => {
+    try {
+      await api.delete(`/tasks/${id}`);
+
+      return id;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
   },
 );
 
@@ -42,6 +67,8 @@ const tasksSlice = createSlice({
       error: null,
     },
     adding: false,
+    editing: false,
+    deleting: false,
   },
 
   extraReducers: {
@@ -73,20 +100,55 @@ const tasksSlice = createSlice({
       state.error.message = action.payload;
     },
 
-    [addTasks.pending]: (state) => {
+    [addTask.pending]: (state) => {
       state.adding = true;
     },
 
-    [addTasks.fulfilled]: (state, action) => {
+    [addTask.fulfilled]: (state, action) => {
       state.adding = false;
       state.tasks.push({
         message: action.meta.arg.message,
       });
     },
 
-    [addTasks.rejected]: (state, action) => {
-      state.loading = false;
+    [addTask.rejected]: (state, action) => {
+      state.adding = false;
       state.error.message = action.payload;
+    },
+
+    [editTask.pending]: (state) => {
+      state.editing = true;
+    },
+
+    [editTask.fulfilled]: (state, action) => {
+      state.editing = false;
+      const checkId = state.items.findIndex((item) => {
+        return item._id === action.payload;
+      });
+      state.items[checkId].message = action.meta.arg.message;
+    },
+
+    [editTask.rejected]: (state, action) => {
+      state.editing = false;
+      state.error.message = action.payload;
+      state.error.failed = true;
+    },
+
+    [deleteTask.pending]: (state) => {
+      state.deleting = true;
+    },
+
+    [deleteTask.fulfilled]: (state, action) => {
+      state.deleting = false;
+      state.items = state.items.filter((item) => {
+        return item._id !== action.meta.arg;
+      });
+    },
+
+    [deleteDraft.rejected]: (state, action) => {
+      state.error.message = action.payload;
+      state.error.failed = true;
+      state.deleting = false;
     },
   },
 });
