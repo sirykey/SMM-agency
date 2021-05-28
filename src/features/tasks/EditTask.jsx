9bcc7ed5b06d3a0f39a63eba9bd0e2928cbbbedd
emin <Magamedi98@mail.ru>
2021-05-28 21:10:42 +0300

    added hovers for deleting and editing tasks

--- src/features/tasks/EditTask.jsx
+++ src/features/tasks/EditTask.jsx
@@ -6,7 +6,7 @@ import {
   DialogActions,
   Button,
   Dialog,
-  CircularProgress,
+  CircularProgress, Tooltip,
 } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import IconButton from '@material-ui/core/IconButton';
@@ -20,7 +20,7 @@ function EditTask({ message, id, role, completed, userId }) {
   const [messageValue, setMessageValue] = useState(message);
 
   const editing = useSelector((state) => state.tasksSlice.editing);
-  const changing = useSelector((state) => state.tasksSlice.changing)
+  const changing = useSelector((state) => state.tasksSlice.changing);
   const handleOpen = () => {
     setOpen(true);
   };
@@ -42,47 +42,49 @@ function EditTask({ message, id, role, completed, userId }) {
     });
   };
 
-  const handleChecked = ({completed, id, userId}) => {
-    dispatch(changeTaskCompleted({completed, id, userId}))
-  }
+  const handleChecked = ({ completed, id, userId }) => {
+    dispatch(changeTaskCompleted({ completed, id, userId }));
+  };
 
   return (
     <>
-      {role === 'ADMIN'? (
-        <IconButton
-          color="primary"
-          edge="end"
-          aria-label="changed"
-          onClick={handleOpen}
-        >
-          <EditIcon />
-        </IconButton>
-      ): changing? <CircularProgress size={16} /> : <input type="checkbox" checked={completed} onChange={() => {
-        handleChecked({completed, id, userId});
-      }}/>}
+      {role === 'ADMIN' ? (
+        <Tooltip title='Изменить задачу'>
+          <IconButton
+            color='primary'
+            edge='end'
+            aria-label='changed'
+            onClick={handleOpen}
+          >
+            <EditIcon />
+          </IconButton>
+        </Tooltip>
+      ) : changing ? <CircularProgress size={16} /> : <input type='checkbox' checked={completed} onChange={() => {
+        handleChecked({ completed, id, userId });
+      }} />}
 
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Изменить черновик</DialogTitle>
         <DialogContent>
           <TextField
             onChange={handleMessageChange}
-            type="text"
-            variant="outlined"
+            type='text'
+            variant='outlined'
             fullWidth
             value={messageValue}
           />
         </DialogContent>
         <DialogActions>
           {editing ? <CircularProgress size={16} /> : <Button
-            color="primary"
-            variant="contained"
+            color='primary'
+            variant='contained'
             onClick={handleAddClick}
             disabled={editing}
           >
             Change task
           </Button>}
 
-          <Button color="secondary" variant="contained" onClick={handleClose}>
+          <Button color='secondary' variant='contained' onClick={handleClose}>
             Cancel
           </Button>
         </DialogActions>
