a7cc679fe94f875ed06f22892e4d07bf90df7e9d
SiriKey <kiryu110@mail.ru>
2021-06-03 23:42:37 +0300

    добавил стили для комментариев и изменил для шапки

--- src/app/styles.js
+++ src/app/styles.js
@@ -16,6 +16,7 @@ export const useSidebarStyles = makeStyles((theme) => ({
 
 export const useHeaderStyles = makeStyles((theme) => ({
   appBar: {
+    position: 'fixed',
     zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(['margin', 'width'], {
       easing: theme.transitions.easing.sharp,
@@ -30,5 +31,29 @@ export const useHeaderStyles = makeStyles((theme) => ({
   },
   root: {
     display: 'flex',
+  },
+}));
+
+export const useCommentsStyles = makeStyles((theme) => ({
+  cardGrid: {
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(5),
+  },
+  paper: {
+    marginTop: theme.spacing(5),
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  btn: {
+    marginRight: theme.spacing(1),
+  },
+  list: {
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+    position: 'relative',
+    overflow: 'auto',
+    maxHeight: 300,
   },
 }));
