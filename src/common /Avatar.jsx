dac9114815998e3a21de4042384113b60793d03a
emin <Magamedi98@mail.ru>
2021-06-03 17:13:29 +0300

    вынес стили в отдельный компонент

--- src/common /Avatar.jsx
+++ src/common /Avatar.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import ProfileAvatar from '@material-ui/core/Avatar';
 import PropTypes from 'prop-types';
+import { useAvatarStyles } from './styles';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    '& > *': {
-      margin: theme.spacing(1),
-      marginRight: 20
-    },
-  },
-}));
-
-function Avatar({imageUrl}) {
-  const classes = useStyles();
+function Avatar({ imageUrl }) {
+  const classes = useAvatarStyles();
 
   return (
     <div className={classes.root}>
-      <ProfileAvatar alt="Remy Sharp" src="https://im0-tub-ru.yandex.net/i?id=68fb829949457e2e74dee578aef5b002&n=13&exp=1"/>
+      <ProfileAvatar
+        alt="Remy Sharp"
+        src="https://im0-tub-ru.yandex.net/i?id=68fb829949457e2e74dee578aef5b002&n=13&exp=1"
+      />
     </div>
   );
 }
