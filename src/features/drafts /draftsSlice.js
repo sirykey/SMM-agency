37435536d81f04561bd9a8d0d17d893344164fb2
emin <Magamedi98@mail.ru>
2021-06-03 17:03:49 +0300

    добавил thunk для удаления черновиков

--- src/features/drafts /draftsSlice.js
+++ src/features/drafts /draftsSlice.js
@@ -1,13 +1,22 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { api } from '../../app/api';
 
-export const fetchDrafts = createAsyncThunk(
-  'drafts/fetchDrafts',
-  async () => {
-    const response = await api.get('/posts')
-    return response.data;
-  }
-)
+export const fetchDrafts = createAsyncThunk('drafts/fetchDrafts', async () => {
+  const response = await api.get('/posts');
+  return response.data;
+});
+
+export const deleteDraft = createAsyncThunk(
+  'drafts/deleteDrafts',
+  async (id, thunkAPI) => {
+    try {
+      await api.delete(`/posts/${id}`);
+      return id;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  },
+);
 
 const draftsSlice = createSlice({
   name: 'drafts',
@@ -38,8 +47,26 @@ const draftsSlice = createSlice({
       state.loading = false;
       state.error.failed = true;
       state.error.message = action.payload;
-    }
-  }
-})
+    },
+
+    [deleteDraft.pending]: (state) => {
+      state.deleting = true;
+      state.error.failed = false;
+    },
+
+    [deleteDraft.fulfilled]: (state, action) => {
+      state.deleting = false;
+      state.drafts = state.items.drafts((draft) => {
+        return draft._id !== action.payload;
+      });
+    },
+
+    [deleteDraft.rejected]: (state, action) => {
+      state.deleting = false;
+      state.error.failed = true;
+      state.error.message = action.payload;
+    },
+  },
+});
 
 export default draftsSlice.reducer;
