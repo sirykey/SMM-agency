c09f7bf9e71563d32d6c209de611dc09c73f82e4
SiriKey <kiryu110@mail.ru>
2021-06-03 16:44:52 +0300

    прописал стили для компонентов

---
+++ src/app/styles.js
@@ -0,0 +1,34 @@
+import { makeStyles } from '@material-ui/core';
+
+const drawerWidth = 240;
+export const useSidebarStyles = makeStyles((theme) => ({
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerContainer: {
+    overflow: 'auto',
+  },
+}));
+
+export const useHeaderStyles = makeStyles((theme) => ({
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  toolbar: {
+    paddingRight: 24,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  root: {
+    display: 'flex',
+  },
+}));
