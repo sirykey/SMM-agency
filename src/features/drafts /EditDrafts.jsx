ff887c08c28848b59268f4c2875146f9bda8c9c9
emin <Magamedi98@mail.ru>
2021-06-05 19:28:19 +0300

    Добавил окно для редактирования текста и кнопку.

--- src/features/drafts /EditDrafts.jsx
+++ src/features/drafts /EditDrafts.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Container, Grid, TextField, Typography } from '@material-ui/core';
+import { Button, Container, Grid, TextField, Typography } from '@material-ui/core';
 import { useEditDraftStyles } from './styles';
-
+import { Editor } from '@tinymce/tinymce-react';
 function EditDrafts() {
   const classes = useEditDraftStyles()
   return (
@@ -12,8 +12,29 @@ function EditDrafts() {
             alignItems='flex-start'
 >
         <Typography variant='h3' align='center' className={classes.form}>Редактиование</Typography>
-        <TextField fullWidth  className={classes.form} type='text' variant='outlined'></TextField>
+        <TextField fullWidth  className={classes.form} type='text' variant='outlined' />
       </Grid>
+      <Editor
+        initialValue=""
+        apiKey="jlz8bac87srss3dre4jzt1fhtk9w6fs6sg8l7ywftd113tv8"
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount'
+          ],
+          toolbar: 'undo redo | formatselect | ' +
+            'bold italic backcolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+        }}
+      />
+      <Button variant="contained" color="primary" className={classes.btn}>
+        Сохранить
+      </Button>
     </Container>
   );
 }
