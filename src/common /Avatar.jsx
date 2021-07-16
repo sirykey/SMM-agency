f5bc4b7af824ce90f1d04f0c2d6e27f516ed3c1f
emin <Magamedi98@mail.ru>
2021-07-16 20:04:36 +0300

    Создал компоненты Аватар и Профайл Аватар ( один из них удалю позже )

--- src/common /Avatar.jsx
+++ src/common /Avatar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import ProfileAvatar from '@material-ui/core/Avatar';
-import PropTypes from 'prop-types';
-import { useCommonStyles } from './styles';
+import Avatar from '@material-ui/core/Avatar';
+import { PropTypes } from 'prop-types';
+import { useAvatarStyles } from './styles';
 
-function Avatar({ imageUrl }) {
-  const classes = useCommonStyles();
+ function ImageAvatars({ size }) {
+  const classes = useAvatarStyles();
 
   return (
     <div className={classes.root}>
-      <ProfileAvatar
+      <Avatar
         alt="Remy Sharp"
-        src="https://yandex-images.clstorage.net/o9nm5g408/acc5f9nDQkxr/yE9EX1FZ-vpd6HN2D2GwJBaRbskwae39QYEz-GAznlHmxPdGynn5O_96OrA9-7SX5KA2jvXSk1uVsk-5J1YNL6jusL8VKYvvWZkUY49l5IXjBKsc4dhdPUVAxoghFyrE42cSkst9PD6qbJrwbdpGZUaR0f1CQfGsl3DOCVLjTTt5CpH8s83rWjw5lNQMEgRHtJGs-DOvT79QZ6HYQwYM0aWmB-T4bW58e62O1-hPNbalklMcQcNQLBSAz0vjM19738gwfhNY6cvcinTFTrDUBOcjXtjBDDw9QOaCGWEXLRMFJlYXe81eTBi_PqbInjf3NsDR_cAj8E9HgU7qcaH9C52KkvxCSdsIveoF1P9w1qdXg85oQ86OXIPl8gqDtt8iFzdVFwtfjO763z8Hqu7VdvSQkE7x0FBuxpNfStAx7cuc-3GNYUiJGy4Lx9VcA-T2xQKe2DLdPl9j1ENqYEU_ExUUV6QrTX7tCFwNVnpsx6RG4QBM46ChjhTRH0ggMgwaXOsiHBEIaNnuCxSkrzBkVbQyn7kyTh5tw9bROsEVz-EE5TfGSK_s3xt8fdUrrEUmhKER75Jikd5kwDwZ0nOuCV_LU_2zGSnZHJm29uwixgVnov6ZEo2sbnDmYPiy9Z9BN3SFVOgtjQ4Lfd6mOV-HR8QBI16Rk4Kv5_Ce6NMATkjvK4B-APm7mW9rJRS8EuT2JpP8GEOd_S-iBaPIAWcNYISkBddIzA9PeE99x-vPtDf1E9F88zBDzmXCH8sCEk977Vijr_MretgN28UEfIC0ZuRxrsgRn43MsnbD2KE2TPF2ZmSEKL2_vPktP9RIbgbGNxPRbPDQACzVIF8r0iG_uc0r0r3S25ipD-oWpj0wBNd30TwbwO9OfiIncLkAVQ5y1Je11vnNzYx4Lw8V6F0mpnTjAWwCI8JfxRM_yxFhXZuvOTDdk1qa2D5bdMatoNend4Du23L9zA0yhUDa4"
+        src="https://img.favpng.com/23/14/13/avatar-computer-icons-logo-png-favpng-cn0QQR4duut7PZicNCY4KtB1H.jpg"
+        className={size}
       />
     </div>
   );
@@ -20,4 +21,4 @@ Avatar.propTypes = {
   imageUrl: PropTypes.string,
 };
 
-export default Avatar;
+export default ImageAvatars;
---
+++ src/common /ProfileAvatar.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Avatar, IconButton, makeStyles } from '@material-ui/core';
+import { PhotoCamera } from '@material-ui/icons';
+
+const useStyles = makeStyles(() => ({
+  account: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  avatar: {
+    width: '120px',
+    height: '120px',
+  },
+  avatarWrapper: {
+    margin: '20px auto',
+    display: 'inline-block',
+    border: '#ccc 1px dashed',
+    padding: '10px',
+    borderRadius: '50%',
+    position: 'relative',
+  },
+  avatarButton: {
+    opacity: 0,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '50%',
+    width: '120px',
+    height: '120px',
+    position: 'absolute',
+    top: '10px',
+    transition: '300ms',
+    backgroundColor: 'rgba(0,0,0,0.45)',
+    '&:hover': {
+      opacity: 1,
+    },
+  },
+  button: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '50%',
+    width: '120px',
+    height: '120px',
+  },
+  subtitle: {
+    fontSize: '12px',
+    textAlign: 'center',
+    padding: '10px 50px',
+    color: '#6a6a6a',
+    marginBottom: '100px',
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
+function ProfileAvatar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.account}>
+      <input
+        accept="image/*"
+        className={classes.input}
+        id="icon-button-file"
+        type="file"
+      />
+
+      <div className={classes.avatarWrapper}>
+        <Avatar className={classes.avatar} />
+        <div className={classes.avatarButton}>
+          <label htmlFor="icon-button-file">
+            <IconButton
+              color="default"
+              aria-label="upload picture"
+              component="span"
+              className={classes.button}
+            >
+              <PhotoCamera />
+            </IconButton>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileAvatar;
