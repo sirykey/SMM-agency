a196bf810b4950820264c2c83c91ab5f8638c891
emin <Magamedi98@mail.ru>
2021-07-16 20:06:08 +0300

    Добавил стили компонента AddDrafts

--- src/features/drafts /styles.js
+++ src/features/drafts /styles.js
@@ -39,24 +39,37 @@ export const useDraftStyles = makeStyles((theme) => ({
   },
 
   btn: {
-    marginTop: 12,
+    marginTop: 5,
   },
 }));
 
 export const useEditDraftStyles = makeStyles((theme) => ({
+  containerForEditDraftsComponent: {},
+
   form: {
-    margin: theme.spacing(1, 0),
+    padding: theme.spacing(4),
+    paddingTop: theme.spacing(4),
   },
+
+  editor: {
+    paddingBottom: theme.spacing(4),
+    paddingLeft: theme.spacing(4),
+    paddingRight: theme.spacing(4),
+  },
+
   root: {
     margin: theme.spacing(1),
     width: theme.spacing(16),
     height: theme.spacing(16),
   },
   cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
+    paddingTop: theme.spacing(5),
+    paddingBottom: theme.spacing(5),
     display: 'flex',
-    flexDirection: 'column',
+  },
+
+  EditingTitle: {
+    marginTop: theme.spacing(12),
   },
 
   containerForButtons: {
@@ -65,7 +78,63 @@ export const useEditDraftStyles = makeStyles((theme) => ({
 
   btn: {
     alignSelf: 'flex-end',
-    marginTop: 12,
     marginLeft: 3,
+  },
+
+  blockForPostImg: {
+    justifyContent: 'center',
+    maxWidth: 500,
+    maxHeight: 700,
+    marginLeft: 2,
+  },
+
+  gridForAddingPictures: {
+    zIndex: 10,
+    position: 'relative',
+    bottom: 350,
+    left: 170,
+    '&:hover': {
+      background: 'black',
+    },
+  },
+
+  picture: {
+    padding: theme.spacing(4),
+  },
+
+  postImg: {
+    width: '100%',
+  },
+}));
+
+export const useAddDraftStyles = makeStyles((theme) => ({
+  root: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+
+  containerForBtns: {
+    paddingTop: theme.spacing(2),
+    paddingRight: theme.spacing(5),
+  },
+
+  btn: {
+    marginTop: 3,
+  },
+
+  addingTitle: {
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(4),
+    marginLeft: theme.spacing(4),
+  },
+
+  form: {
+    padding: theme.spacing(5),
+  },
+
+  editor: {
+    paddingBottom: theme.spacing(5),
+    paddingLeft: theme.spacing(5),
+    paddingRight: theme.spacing(5),
   },
 }));
