e478cce847c60cbd0204b0e4632f840be217a4ed
SiriKey <kiryu110@mail.ru>
2021-07-17 12:06:42 +0300

    добавил вывод задачи для одного пользователя

--- src/features/tasks/TasksList.jsx
+++ src/features/tasks/TasksList.jsx
@@ -4,15 +4,12 @@ import TasksItem from './TasksItem';
 import { fetchOneTask, fetchTasks } from './tasksSlice';
 import { useDraftStyles } from '../drafts /styles';
 import { Container, Grid, List, Paper, Typography } from '@material-ui/core';
-import authSlice from '../auth/authSlice';
 
 function TasksList(props) {
   const classes = useDraftStyles();
   const tasks = useSelector((state) => state.tasksSlice.tasks);
   const authUser = useSelector(state => state.authSlice.id)
-if (authUser !== undefined) {
-  console.log(authUser);
-}
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchTasks());
