1af335b0eb24e9681f372556a98dfdd48aceafcc
emin <Magamedi98@mail.ru>
2021-07-19 10:22:06 +0300

    Доработал бэйдж. Добавил dispatch, вызвал setCompleted, который меняет свойство completed.

--- src/common /Badge.jsx
+++ src/common /Badge.jsx
@@ -2,26 +2,46 @@ import React, { useState } from 'react';
 import { Fab } from '@material-ui/core';
 import { Menu, MenuItem } from '@material-ui/core';
 import PropTypes from 'prop-types';
+import { useBageStyles } from './styles';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCompleted } from '../features/drafts /draftsSlice';
+import { Alert } from '@material-ui/lab';
+import Snackbar from '@material-ui/core/Snackbar';
 
-function Badge() {
+function Badge({id, isItDraft}) {
+  const dispatch = useDispatch()
+  const failed = useSelector(state => state.draftsSlice.failed)
   const [openMenu, setOpenMenu] = useState(null);
   const handleClick = (e) => {
     setOpenMenu(e.currentTarget);
   };
-
+  
   const handleClose = () => {
     setOpenMenu(null);
   };
+
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(null)
+  }
+
+  const handleSetCompleted = () => {
+     dispatch(setCompleted(id)).then(() => {
+       setOpenSnackbar(true)
+     })
+  }
+
+  const classes = useBageStyles();
+
   return (
     <div>
       <Fab
         variant="extended"
-        size="small"
-        color="secondary"
+        color={isItDraft ? 'primary' : 'secondary'}
         aria-label="add"
         onClick={handleClick}
+        className={classes.btn}
       >
-        Черновик
+        {isItDraft ? 'Черновик' : 'Пост'}
       </Fab>
       <Menu
         anchorEl={openMenu}
